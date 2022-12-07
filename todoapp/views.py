5b9f7bd6f7bee713c7747983e0f1ba6c9e42c2cb
vidhisaha03 <vidhisaha02@gmail.com>
2022-12-07 17:14:10 +0530

    intial commit

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import TodoListItem 
 from django.http import HttpResponseRedirect 
+from django import forms
 
-from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.decorators import login_required 
+
+
 # Create your views here.
 def index(request):
+    
     return render(request, 'index.html')
 
 
@@ -25,6 +30,10 @@ def addTodoView(request):
     new_item.content = request.POST.get('content')
     new_item.date1 = request.POST.get('date1')
     new_item.priority = request.POST.get('priority')
+    new_item.shifts = request.POST.get('shifts')
+    new_item.passengers = request.POST.get('passengers')
+    new_item.Ac_type = request.POST.get('Ac_type')
+    new_item.date_time_input = request.POST.get('calender')
     new_item.save()
     return HttpResponseRedirect('/todo/') 
     
@@ -33,3 +42,5 @@ def deleteTodoView(request, i):
     y = TodoListItem.objects.get(id= i)
     y.delete()
     return HttpResponseRedirect('/todo/') 
+
+
